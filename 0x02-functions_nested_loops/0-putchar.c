74ff055e2aa73ad6a25b1caad6396d05a216c41c
JAPH254 <omanyujapheth@gmail.com>
2023-09-13 05:32:57 +0300

    functions basics in c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,12 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - function
- * Return: 0
- */
+  *main - Entry point
+  *Return:  Always 0 (Success)
+**/
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
-
 }
-
