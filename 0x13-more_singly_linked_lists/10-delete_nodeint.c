cc24c9424f1e1a0364651c7ef4ff5a00c82e7483
JAPH254 <omanyujapheth@gmail.com>
2023-10-24 20:34:57 +0300

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,22 +1,21 @@
 #include "lists.h"
+
 /**
  * delete_nodeint_at_index - Entry Point
  * @head: head
  * @index: index
  * Return: 0
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
+
 {
 	unsigned int count = 0;
-
 	listint_t *copy;
 	listint_t *temp;
-
 	if (*head == NULL)
 		return (-1);
-
 	copy = *head;
-
 	if (index == 0)
 	{
 		temp = copy->next;
@@ -24,7 +23,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = temp;
 		return (1);
 	}
-
 	while (copy != NULL)
 	{
 		if (count + 1 == index)
